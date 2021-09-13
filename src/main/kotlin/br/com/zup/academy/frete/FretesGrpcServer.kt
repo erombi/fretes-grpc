e7f454c62367c0379dde16dc0c3c6ae756b23938
Eduardo Rombi <eduardo.rombi@zup.com.br>
2021-09-13 10:31:15 -0300

    Adicionando validações

--- src/main/kotlin/br/com/zup/academy/frete/FretesGrpcServer.kt
+++ src/main/kotlin/br/com/zup/academy/frete/FretesGrpcServer.kt
@@ -2,7 +2,12 @@ package br.com.zup.academy.frete
 
 import br.com.zup.academy.CalculaFreteRequest
 import br.com.zup.academy.CalculaFreteResponse
+import br.com.zup.academy.ErroDetails
 import br.com.zup.academy.FreteServiceGrpc
+import com.google.protobuf.Any
+import com.google.rpc.Code
+import io.grpc.Status
+import io.grpc.protobuf.StatusProto
 import io.grpc.stub.StreamObserver
 import jakarta.inject.Singleton
 import org.slf4j.Logger
@@ -19,6 +24,37 @@ class FretesGrpcServer(
 
         request?.let {
             logger.info("Calculando frete da request: $request")
+
+            if (it.cep == null || it.cep.isBlank()) {
+                val error = Status.INVALID_ARGUMENT.withDescription("CEP não informado !")
+                    .asRuntimeException()
+
+                responseObserver?.onError(error)
+                return
+            }
+
+            if (!it.cep.matches("[0-9]{5}-[\\d]{3}".toRegex())) {
+                val error = Status.INVALID_ARGUMENT.withDescription("CEP informado não é válido !")
+                    .augmentDescription("formato esperado: 00000-000")
+                    .asRuntimeException()
+
+                responseObserver?.onError(error)
+                return
+            }
+
+            if (it.cep.endsWith("333")) {
+                val statusProto = com.google.rpc.Status.newBuilder()
+                    .setMessage("Usuário não pode acessar o recurso !")
+                    .setCode(Code.PERMISSION_DENIED.number)
+                    .addDetails(Any.pack(ErroDetails.newBuilder()
+                        .setCode(401)
+                        .setMessage("Token expirado !")
+                        .build()))
+                    .build()
+
+                val status = StatusProto.toStatusRuntimeException(statusProto)
+                responseObserver?.onError(status)
+            }
 
             val frete = request.toModel()
 
